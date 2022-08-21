7a8204da189937eadf2212bdfe88081f4e392219
nicogioino <nicolas.gioino@ing.austral.edu.ar>
2022-08-20 23:43:28 -0300

    Added .env file read on service startup

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import (
 	"chain-vote-api/repositories"
 	"chain-vote-api/security"
 	"chain-vote-api/services"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	r := gin.Default()
 
+	err := godotenv.Load(".vscode/.env")
+	
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
 	repositories.ConnectDataBase()
+
+	
 
 	public := r.Group("/api")
 	public.POST("/register", services.Register)
