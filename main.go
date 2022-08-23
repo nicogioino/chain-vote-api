e36ad8eddffeee239ab54b7fb28d33038f0581a9
nicogioino <nicolas.gioino@ing.austral.edu.ar>
2022-08-23 19:08:19 -0300

    Refactored router logic into its own file to decouple logic

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"chain-vote-api/middlewares"
 	"chain-vote-api/repositories"
-	"chain-vote-api/security"
-	"chain-vote-api/services"
+	"chain-vote-api/router"
 	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -13,27 +13,16 @@ import (
 func main() {
 
 	r := gin.Default()
+	router.InitializeRouter(r)
 
 	// Load environment variables if .env file exists
 	err := godotenv.Load(".vscode/.env")
-	
+
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
 
 	repositories.ConnectDataBase()
 
-	public := r.Group("/api")
-	public.POST("/register", services.Register)
-	public.POST("/login", security.Login)
-
-	protected := r.Group("/api")
-	protected.Use(middlewares.JwtAuthMiddleware())
-	protected.GET("/me", services.CurrentUser)
-	protected.PUT("/register-address", services.RegisterETHAddress)
-	protected.POST("/election", services.CreateElection)
-	protected.GET("/election/:id", services.GetElectionById)
-	protected.GET("/elections", services.GetAllElections)
-
-	r.Run(":8080")
+	log.Fatalln(r.Run(":8080"))
 }
